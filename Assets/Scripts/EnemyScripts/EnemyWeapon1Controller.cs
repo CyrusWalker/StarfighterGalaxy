d59b8438a7faac50d98915c6b6935e6225707fa4
Cyrus Walker <cyrus.walker831@myci.csuci.edu>
2023-04-11 10:41:44 -0700

    More work on enemies shooting. Made the enemies' use of weapons mirror the way the player uses them. Currently rewritting scripts to resolve an unknown reference error

--- Assets/Scripts/EnemyScripts/EnemyWeapon1Controller.cs
+++ Assets/Scripts/EnemyScripts/EnemyWeapon1Controller.cs
@@ -4,21 +4,15 @@ using UnityEngine;
 
 public class EnemyWeapon1Controller : MonoBehaviour
 {
-    [SerializeField] private Weapon1 weapon;
-    [SerializeField] private float randomWaitTime;
-
+    // Start is called before the first frame update
     void Start()
     {
-        randomWaitTime = Random.Range(0.5f, 3f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        randomWaitTime -= Time.deltaTime;
-        if(randomWaitTime <= 0) {
-            weapon.Shoot();
-        }
-        randomWaitTime = Random.Range(0.5f, 3f);
+        
     }
 }
