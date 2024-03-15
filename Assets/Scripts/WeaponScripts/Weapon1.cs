72dd589922f7f5ef72111dfc460d8f5650fa9ed8
Marcus Meijer <marcus.meijer417@myci.csuci.edu>
2024-03-15 13:45:45 -0700

    Added randomized SFX for enemies spawning

--- Assets/Scripts/WeaponScripts/Weapon1.cs
+++ Assets/Scripts/WeaponScripts/Weapon1.cs
@@ -6,11 +6,15 @@ public class Weapon1 : MonoBehaviour
 {
     public Transform firePoint1;
     public GameObject bulletPrefab;
+    public AudioSource shootSFX;
 
     public void Shoot()
     {
-
         if (gameObject.activeSelf)
+        {
+            if (shootSFX != null)
+                shootSFX.Play();
             Instantiate(bulletPrefab, firePoint1.position, firePoint1.rotation);
+        }
     }
 }
--- Assets/Scripts/WeaponScripts/Weapon3.cs
+++ Assets/Scripts/WeaponScripts/Weapon3.cs
@@ -8,11 +8,14 @@ public class Weapon3 : MonoBehaviour
     public Transform firePoint2;
     public Transform firePoint3;
     public GameObject bulletPrefab;
+    public AudioSource shootSFX;
 
     public void Shoot()
     {
         if (gameObject.activeSelf)
         {
+            if (shootSFX != null)
+                shootSFX.Play();
             Instantiate(bulletPrefab, firePoint1.position, firePoint1.rotation);
             Instantiate(bulletPrefab, firePoint2.position, firePoint2.rotation);
             Instantiate(bulletPrefab, firePoint3.position, firePoint3.rotation);
